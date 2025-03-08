dadd013a44c1630892fe5899375ab7ce75cfc000
kuriousdesign <gardner.761@gmail.com>
2025-03-08 15:15:43 -0800

    prep for tailwind upgrade

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <nav>
+          <h1>Conor Byrne</h1>
+          {children}
+        </nav>
       </body>
     </html>
   );
